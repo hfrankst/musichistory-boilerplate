cf3651c58012476ef9158237ba7901a94984243b
Harper Frankstone <Harper Frankstone>
2017-02-06 16:11:45 -0600

    working on the for loop to manipulate the array of songs

--- songs.js
+++ songs.js
@@ -1,11 +1,17 @@
 var songs = [
-	songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-	songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-	songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-	songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-	songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+	"Legs > by Z*ZTop on the album Eliminator",
+	"The Logical Song > by Supertr@amp on the album Breakfast in America",
+	"Another Brick in the Wall > by Pink Floyd on the album The Wall",
+	"Welcome to the Jungle > by Guns & Roses on the album Appetite for Destruction",
+	"Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill"
 ];
-
+console.log("Songs to be added by loop: ", songs);
+	//this loop will take out any incorrect characters in the song strings..so far it's not working
 for (var i = 0; i < songs.length; i++){
-
+	var charReplace = songs[i].replace('>', '-').replace('*', '').replace('@', '').replace('!', '');
+	console.log("Songs: ", charReplace);
+	document.getElementsByClassName('right-side')[0].innerHTML = charReplace[i];
+	console.log("Adjusted song titles: ", charReplace);
 };
+	//the following will assign the corrected array of strings to a new variable that will be pushed to the main content area
+// var updatedSongs = charReplace;
