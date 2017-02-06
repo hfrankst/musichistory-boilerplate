3c05ece594938aa39ef35a2ece8865cc3046f284
Harper Frankstone <Harper Frankstone>
2017-02-06 17:38:48 -0600

    finished but need to add a line break to the newly added songs

--- songs.js
+++ songs.js
@@ -9,9 +9,12 @@ console.log("Songs to be added by loop: ", songs);
 	//this loop will take out any incorrect characters in the song strings..so far it's not working
 for (var i = 0; i < songs.length; i++){
 	var charReplace = songs[i].replace('>', '-').replace('*', '').replace('@', '').replace('!', '');
+	
+	// console.log("The line break", lineBreak);
+	document.getElementsByClassName('right-side')[0].append(charReplace);
 	console.log("Songs: ", charReplace);
-	document.getElementsByClassName('right-side')[0].innerHTML = charReplace[i];
-	console.log("Adjusted song titles: ", charReplace);
 };
+
+
 	//the following will assign the corrected array of strings to a new variable that will be pushed to the main content area
 // var updatedSongs = charReplace;
