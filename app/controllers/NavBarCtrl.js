7caa802916d4176b24bab9dd9b8db8539b8169f1
Harper Frankstone <Harper Frankstone>
2017-03-07 11:34:06 -0600

    still setting up

--- app/controllers/NavBarCtrl.js
+++ app/controllers/NavBarCtrl.js
@@ -1,11 +1,21 @@
 "use strict";
 
-app.controller("NavBarCtrl", function($scope, $window, $location, $routeParams){
+app.controller("NavBarCtrl", function($scope, $window, SearchTermData){
 console.log("NavBarCtrl");
 
-	$('#add-music').click(function() {
-		$window.location.href = "#!/form";
-	});
+ $scope.searchText = SearchTermData;
+    $scope.isLoggedIn = false;
 
+    firebase.auth().onAuthStateChanged(function (user) {
+    	if(user){
+    		$scope.isLoggedIn = true;
+    		console.log("currentUser logged in", $scope.isLoggedIn);
+    	} else {
+    		$scope.isLoggedIn = false;
+    		console.log("currentUser logged in", $scope.isLoggedIn);
+    		//$window.location forces the page to completely reload
+    		$window.location.href = "#!/login";
+    	}
+    });
 	
 });
